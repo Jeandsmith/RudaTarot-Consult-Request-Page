8df8ea6b5c3d94ebfd1cb904ec6c346590e524c2
Abreu, James S <jeandsmith@protonmail.com>
2020-01-13 19:18:05 -0500

    Redone work

--- public/db.js
+++ public/db.js
@@ -1,13 +1,34 @@
 let con = require('postgres');
 
 // Create instance of connection
-
+const sql = con('postgres://root:root@localhost:5432/ClientDB', {
+    connection: {
+        application_name: 'app.js'
+    }
+});
 
 // Add new client to the database
 module.exports.postClient = (req, res) => {
-    // Do something with the post 
     
-    // req.app.set('added', true);
-    // // Send the client to thank you page
-    // res.redirect('thank-you');
+    // I dont need the button, so remove from packet
+    delete req.body.button;
+
+    /** 
+     * Asynchronus code
+     */
+    (async () => {
+        // sql `
+        //     insert into client ${
+        //         sql(req.body)
+        //     }
+        // `
+
+        sql `insert into client ${
+            sql(req.body)
+        }`;
+
+        console.log("Done Inserting");
+    })();
+
+    res.redirect('thank-you');
 };
